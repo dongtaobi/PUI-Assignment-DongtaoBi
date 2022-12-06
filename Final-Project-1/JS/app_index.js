3055533e0ae1b9fc358ba5d56882ad3cf2fb2513
joefung630 <dongtaobi@gmail.com>
2022-12-05 22:46:38 -0500

    Update
    Update

--- Final-Project-1/JS/app_index.js
+++ Final-Project-1/JS/app_index.js
@@ -12,11 +12,9 @@ function toggleDetails() {
 }
 
 // Get the icon element
-let icon = document.querySelector("#details");
+let icon = document.querySelectorAll(".key-expand");
 console.log(icon);
 // Attach a click event listener to the icon
 icon.addEventListener("click", function() {
-  // Call the function when the icon is clicked
   toggleDetails();
-  console.log("details expanded");
-});
+  });
